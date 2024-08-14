13f36fbf5c9e6240656bc59b7a15be6970633fc7
Seunghyun Park <sh.park@hdjunction.co.kr>
2024-08-14 10:11:12 +0900

    Fix build settings

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,19 +1,20 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
 plugins {
-	kotlin("jvm") version "1.7.10"
-	kotlin("plugin.spring") version "2.7.3"
-	id("org.springframework.boot") version "3.3.2"
-	id("io.spring.dependency-management") version "1.1.6"
-	kotlin("plugin.jpa") version "1.9.24"
+	id("io.spring.dependency-management")
+	id("org.springframework.boot")
+
+	kotlin("plugin.spring")
+	kotlin("plugin.jpa")
+	kotlin("kapt")
+	kotlin("jvm")
+
+	application
+	idea
 }
 
 group = "com.example"
 version = "0.0.1-SNAPSHOT"
-
-java {
-	toolchain {
-		languageVersion = JavaLanguageVersion.of(17)
-	}
-}
 
 repositories {
 	mavenCentral()
@@ -25,18 +26,23 @@ dependencies {
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	developmentOnly("org.springframework.boot:spring-boot-devtools")
-	runtimeOnly("com.mysql:mysql-connector-j")
+	runtimeOnly("mysql:mysql-connector-java")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
-kotlin {
-	compilerOptions {
-		freeCompilerArgs.addAll("-Xjsr305=strict")
-	}
-}
 
-tasks.withType<Test> {
-	useJUnitPlatform()
+tasks {
+	withType<KotlinCompile> {
+		kotlinOptions {
+			freeCompilerArgs = listOf("-Xjsr305=strict")
+			jvmTarget = "11"
+			allWarningsAsErrors = false
+		}
+	}
+
+	test {
+		useJUnitPlatform()
+	}
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,19 @@
 rootProject.name = "jpa-event-listener"
+
+pluginManagement {
+    val springBootVersion: String by settings // from gradle.properties
+    val kotlinVersion: String by settings // from gradle.properties
+
+    // 플러그인 버전은 여기에만 명시, 하위 프로젝트에서는 버전 명시 없이 사용!
+    plugins {
+        // Spring Boot for Kotlin
+        id("io.spring.dependency-management") version "1.0.13.RELEASE"
+        id("org.springframework.boot") version springBootVersion
+        kotlin("jvm") version kotlinVersion
+        kotlin("plugin.spring") version kotlinVersion
+        // Etc
+        kotlin("plugin.jpa") version kotlinVersion
+        kotlin("kapt") version kotlinVersion
+        kotlin("plugin.noarg") version kotlinVersion
+    }
+}
